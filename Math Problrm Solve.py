d308acbd2a5b91fa1a4db817758f6686b96fba59
Yash24may <138605489+Yash24may@users.noreply.github.com>
2023-07-16 23:38:50 -0700

    Add files via upload

---
+++ Math Problrm Solve.py
@@ -0,0 +1,5 @@
+print("Let's Do yout Maths Homework")
+problem=input("Enter a math problem, or 'e' to Exit:")
+while(problem!= "e"):
+    print(eval(problem))
+    problem = input("Enter another math problem, or 'e' to Exit:")
